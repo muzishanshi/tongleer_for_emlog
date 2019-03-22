6bd38e93a464e51b4f301d582e429b8c7fc68f7d
TypechoShop <typechoshop@qq.com>
2019-03-22 12:31:06 +0800

    Diamond

--- footer.php
+++ footer.php
@@ -35,7 +35,7 @@ if(!defined('EMLOG_ROOT')) {exit('error!');}
 .pjax_loading {position: fixed;top: 45%;left: 45%;display: none;z-index: 999999;width: 124px;height: 124px;background: url('<?php echo TEMPLATE_URL; ?>assets/images/pjax_loading.gif') 50% 50% no-repeat;}
 .pjax_loading1 {position: fixed;top: 0;left: 0;z-index: 999999;display: none;width: 100%;height: 100%;opacity: .2}
 </style>
-<script src="https://cdn.bootcss.com/jquery.pjax/1.9.5/jquery.pjax.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.pjax/2.0.1/jquery.pjax.min.js"></script>
 <script type="text/javascript" language="javascript">
 $(function() {
 	ajaxComment();
